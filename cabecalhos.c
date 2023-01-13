968847a36ef0b59f41c8d0f4f933407beaaaee8c
davieduardo001 <ed.miranda@tutanota.com>
2023-01-14 08:14:17 +0900

    null

---
+++ cabecalhos.c
@@ -0,0 +1,16 @@
+//cabecalhos indicam onde o compilador pode
+//procurar por funcoes externas ao programa
+//as extencoes do cabecalho sao .h se localizando
+//dentro das variaveis de ambiente ja compliladas, ou
+//outros diretorios.
+//libs ja compiladas tem a extencao .so, .sa, .a ou .dll
+#include <stdio.h>
+#include <stdlib.h>
+#include <lib_local.h>
+#include </sys/lib/WMlib.h>
+
+int main (int argc, char *argv[])
+{
+    
+    return 0;
+}
