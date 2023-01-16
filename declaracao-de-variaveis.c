53da1f1d278b3da2c90c2d16b4d7c761ac5e8bda
davieduardo001 <ed.miranda@tutanota.com>
2023-01-16 13:24:25 +0100

    .

---
+++ declaracao-de-variaveis.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+
+void main()
+{
+  float coefa, coefb, coefc;
+  float delta;
+  float raiz1, raiz2;
+
+  printf("\tEntre com os valores abaixo como solicitado.");
+  printf("\tDigite o Coeficiente a=\n");
+  scanf("%f", &coefa);
+...
+}
+
